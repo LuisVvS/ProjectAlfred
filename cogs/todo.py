fe953cc3acd8a578beee30f19acb136e971d1a4f
LuisVvS <luluisv7@gmail.com>
2025-01-19 18:37:44 -0300

    added all the CRUD methods, and works fine

--- cogs/todo.py
+++ cogs/todo.py
@@ -10,32 +10,39 @@ class TodoCog(commands.Cog):
     async def echo(self, ctx, *, arg):
         await ctx.send(f"You said {arg}")
 
+    #change the name of the command to "todo_add" for "add"
     @commands.command(name="add")
     async def todo_add(self, ctx,*, arg):
-        
+
+        #pass the argumento to add a task
         create_task(arg)
 
         await ctx.send(f"added {arg} to the list")
 
+
+    #change the name of the command to "todo_delete" for "delete"
     @commands.command(name="delete")
     async def todo_delete(self,ctx, *, arg):
-        ...
 
+        delete_task(arg)
+        
+        await ctx.send(f"Task {arg} deleted succesfully")
+
+    #change the name of the command to "todo_update" for "update"
     @commands.command(name="update")
-    async def todo_update(self, ctx, *, arg):
-        ...
+    async def todo_update(self, ctx, task_id: int, *, nome : str):
+        
+        update_task(task_id,nome)
+        await ctx.send(f"Task number {task_id} updated succesfully")
 
+    #change the name of the command to "todo_show" for "show"
     @commands.command(name="show")
     async def todo_show(self, ctx):
         await ctx.send("------ Lista de tarefas ------")
 
-        cursor = connection().cursor()
+        lista = show_task()
 
-        cursor.execute("SELECT * FROM Todo")
-
-
-        rows = cursor.fetchall()
-
-        for i in rows:
+        for i in lista:
             await ctx.send(i)
+
         await ctx.send("------------------------------")
--- database/models.py
+++ database/models.py
@@ -1,14 +1,41 @@
 from database.DB_connection import connection
 def create_task(arg):
+                        con = connection()
+                        cursor = con.cursor()
+
+                        cursor.execute("INSERT INTO Todo(nome) VALUES(%s)", (arg, ))
+
+                        con.commit()
+
+                        cursor.close()
+                        con.close()
+
+def update_task(arg,nome):
+                        con = connection()
+                        cursor = con.cursor()
+
+                        cursor.execute("UPDATE Todo SET nome = %s WHERE taskid = %s", (nome, arg))
+                        con.commit()
+
+                        con.close()
+                        cursor.close()
+
+def delete_task(arg):
+                        con = connection()
+                        cursor = con.cursor()
+
+                        cursor.execute("DELETE FROM Todo WHERE taskid = %s", (arg, ))
+                        con.commit()
+
+                        con.close()
+                        cursor.close()
+
+def show_task():
                         cursor = connection().cursor()
 
-                        cursor.execute("INSERT INTO Todo(nome) VALUES(%s)", (arg))
+                        cursor.execute("SELECT * FROM Todo")
 
-                        connection().commit()
+                        rows = cursor.fetchall()
 
-def update_task(arg):
-                        ...
-def delete_task(arg):
-                        ...
-def show_task(arg):
-                        ...
+                        return rows
+                        
